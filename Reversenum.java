64e6b5fe2fbbf1ccbe7e415b8866e2461fc8b58c
Parkxit <147944739+Parkxit@users.noreply.github.com>
2024-07-19 17:44:24 +0530

    Update Reversenum.java

--- Reversenum.java
+++ Reversenum.java
@@ -1,8 +1,8 @@
-package FIrstAssignment;
+
 
 import java.util.Scanner;
 
-public class Reversenum {
+ class Reversenum {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
