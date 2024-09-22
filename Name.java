3053ec7d78deb74cfa566c90067266ccc1b7e3b4
Parkxit <147944739+Parkxit@users.noreply.github.com>
2024-09-22 08:00:24 +0530

    syntax errors fixed 1) Name.java

--- Name.java
+++ Name.java
@@ -1,14 +1,13 @@
 
-
 import java.util.Scanner;
 
-class Name {
+public class Name {
     public static void main(String[] args) {
         String name;
         System.out.print("Enter name: ");
         Scanner sc=new Scanner(System.in);
         name=sc.nextLine();
-        System.out.println("Hello "+ name+" .Nice to meet you.How are you doing?");
+        System.out.println("Hello "+ name+". Nice to meet you. How are you doing?");
     }
 }
 
