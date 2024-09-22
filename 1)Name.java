ca4c5aa3215e54df628a22f2e69b0d65e077ec81
Parkxit <147944739+Parkxit@users.noreply.github.com>
2024-09-22 08:02:30 +0530

    Add files via upload

---
+++ 1)Name.java
@@ -0,0 +1,14 @@
+package FirstAssignment;
+
+import java.util.Scanner;
+
+  class Name {
+    public static void main(String[] args) {
+        String name;
+        System.out.print("Enter name: ");
+        Scanner sc=new Scanner(System.in);
+        name=sc.nextLine();
+        System.out.println("Hello "+ name+". Nice to meet you. How are you doing?");
+    }
+}
+
